4c9892a9d4d4edd062815997db9d783c11395ac7
TandileN <107769142+Tandile-03@users.noreply.github.com>
2023-02-27 03:20:19 +0200

    swap turns and set board

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const X_CLASS ='x'
 const O_CLASS = 'circle'
 const cellElements = document.querySelector('[data-cell]')
+const board = document.getElementById('board')
 
-cellElements.forEach(cell =>{
+startGame()
+function startGame(){
+    circleTurn =false
+    cellElements.forEach(cell =>{
     cell.addEventListener('click', handleClick, {once:true})
 })
+}
 
 function handleClick(e){
     //console.log('clicked')
+    const cell = e.target
+    const currentClass = circleTurn ? O_CLASS : X_CLASS
+    placeMark(cell, currentClass) 
+    swapTurns()
+    setBoardHover()
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn
+}
+
+function setBoardHover(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(O_CLASS)
+    if (circleTurn){
+        board.classList.add(O_CLASS)
+    }else{
+        board.classList.add(X_CLASS)
+    }
 }
