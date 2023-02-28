19982f3c5da59b86b4d4c8d0ea31df60d361fd4b
TandileN <107769142+Tandile-03@users.noreply.github.com>
2023-02-28 07:56:12 +0200

    fixing swap turns bugs

--- script.js
+++ script.js
@@ -13,57 +13,61 @@ const WINNING_COMBINATIONS =[
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
-const winningMessageTextElement= document.querySelector('[data-winning-message-text]')
 const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement= document.querySelector('[data-winning-message-text]')
+
 let circleTurn
 
-startGame()
+cellElements.forEach(cell =>
+    {cell.addEventListener('click', handleClick, {once: true})
+})
+//startGame()
 
-restartButton.addEventListener('click', startGame)
+// restartButton.addEventListener('click', startGame)
 
-function startGame(){
-    circleTurn =false
-    cellElements.forEach(cell =>{
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(O_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, {once: true})
-    })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
-}
+// function startGame(){
+//     circleTurn =false
+//     cellElements.forEach(cell =>{
+//         cell.classList.remove(X_CLASS)
+//         cell.classList.remove(O_CLASS)
+//         cell.removeEventListener('click', handleClick)
+//         cell.addEventListener('click', handleClick, {once: true})
+//     })
+//   setBoardHoverClass()
+//   winningMessageElement.classList.remove('show')
+// }
 
 function handleClick(e){
     const cell = e.target
     const currentClass = circleTurn ? O_CLASS : X_CLASS
     placeMark(cell, currentClass)
-    if(checkWin(currentClass)){
-        endGame(false)
-    }else if(checkDraw()){
-        endGame(true)      
-    }
-    else{
+    // if(checkWin(currentClass)){
+    //     endGame(false)
+    // }else if(checkDraw()){
+    //     endGame(true)      
+    // }
+    // else{}
     swapTurns()
-    setBoardHoverClass()
-    }
+    //setBoardHoverClass()
+    
 }
 
-function endGame(draw){
-    if(draw){
-        winningMessageTextElement.innerText ='Draw!'
-    }else{
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : 
-        "X's"} Wins!`
-    }
-    winningMessageElement.classList.add('show')
-}
+// function endGame(draw){
+//     if(draw){
+//         winningMessageTextElement.innerText ='Draw!'
+//     }else{
+//         winningMessageTextElement.innerText = `${circleTurn ? "O's" : 
+//         "X's"} Wins!`
+//     }
+//     winningMessageElement.classList.add('show')
+// }
 
-function checkDraw(){
-    return [...cellElements].every(cell =>{
-        return cell.classList.contains(X_CLASS) ||
-        cell.contains(O_CLASS)
-    })
-}
+// function checkDraw(){
+//     return [...cellElements].every(cell =>{
+//         return cell.classList.contains(X_CLASS) ||
+//         cell.contains(O_CLASS)
+//     })
+// }
 
 function placeMark(cell, currentClass){
     cell.classList.add(currentClass)
@@ -73,21 +77,21 @@ function swapTurns(){
     circleTurn = !circleTurn
 }
 
-function setBoardHoverClass(){
-    board.classList.remove(X_CLASS)
-    board.classList.remove(O_CLASS)
-    if (circleTurn){
-        board.classList.add(O_CLASS)
-    }else{
-        board.classList.add(X_CLASS)
-    }
-}
+// function setBoardHoverClass(){
+//     board.classList.remove(X_CLASS)
+//     board.classList.remove(O_CLASS)
+//     if (circleTurn){
+//         board.classList.add(O_CLASS)
+//     }else{
+//         board.classList.add(X_CLASS)
+//     }
+// }
 
-function checkWin(currentClass){
-    return WINNING_COMBINATIONS.some(combination =>{
-        return combination.every(index =>{
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
-}
+// function checkWin(currentClass){
+//     return WINNING_COMBINATIONS.some(combination =>{
+//         return combination.every(index =>{
+//             return cellElements[index].classList.contains(currentClass)
+//         })
+//     })
+// }
 
